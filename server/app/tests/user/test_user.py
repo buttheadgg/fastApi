5ac8f9f0ed2718ce80f6fe134571b7d4d6130a53
buttheadgg <timofey11229@gmail.com>
2022-06-30 21:01:01 +0300

    add test auth

--- server/app/tests/user/test_user.py
+++ server/app/tests/user/test_user.py
@@ -1,27 +1,62 @@
 import pytest
 import random
 import string
+
 from fastapi.testclient import TestClient
+
 from app.main import app
 
 client = TestClient(app)
 
+
 def random_string(length):
     return ''.join(random.choice(string.ascii_lowercase) for i in range(length))
 
-token = None
-email = random_string(5) + '@gmail.com'
-username = random_string(5)
-password = random_string(5)
 
 def test_sign_up():
-    global token
-    response = client.post("auth/sign-up", json={'email': email, 'username': username, 'password': password})
-    assert response.status_code == 200
-    response = client.post("auth/sign-in", data={'username': username, 'password': password})
-    assert response.status_code == 200
-    token = response.json()['access_token']
-def test_token():
-    response = client.get("auth/user", headers={'Authorization': 'Bearer ' + token})
-    assert response.status_code == 200
-    assert response.json()['email'] == email and response.json()['username'] == username
+    data = [
+        {
+            'request': {'email': random_string(5) + '@gmail.com', 'username': random_string(5), 'password': random_string(5)},
+            'response': 200
+        },
+    ]
+    for d in data:
+        # reg new user
+        response = client.post("auth/sign-up", json=d['request'])
+        assert response.status_code == d['response']
+        
+
+def test_sign_in():
+    data = [
+        {
+            'request': {'email': random_string(5) + '@gmail.com', 'username': random_string(5), 'password': random_string(5)},
+            'response': 200
+        },
+    ]
+    for d in data:
+        # reg new user
+        response = client.post("auth/sign-up", json=d['request'])
+        assert response.status_code == d['response']
+        # login
+        response = client.post("auth/sign-in", data=d['request'])
+        assert response.status_code == d['response']
+        assert isinstance(response.json()['access_token'], str)
+
+def test_get_user():
+    data = [
+            {
+                'request': {'email': random_string(5) + '@gmail.com', 'username': random_string(5), 'password': random_string(5)},
+                'response': 200
+            },
+        ]
+    for d in data:
+        # reg new user
+        response = client.post("auth/sign-up", json=d['request'])
+        assert response.status_code == d['response']
+        assert isinstance(response.json()['access_token'], str)
+        # get token
+        token = response.json()['access_token']
+        # get user info
+        response = client.get("auth/user", headers={'Authorization': 'Bearer ' + token})
+        assert response.status_code == d['response']
+        assert response.json()['email'] == d['request']['email'] and response.json()['username'] == d['request']['username']
